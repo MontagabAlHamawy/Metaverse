eec99a4bebe538f5ba5bd8a2f1d8df8892107f03
MontagabAlHamawy <ma0152260@gmail.com>
2023-10-26 14:30:16 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
-};
+  output: 'export',
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
